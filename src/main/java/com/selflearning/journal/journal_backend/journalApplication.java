162decdd785aff99dac159c0c647531826943425
Rimon <rimonruet@gmail.com>
2025-04-20 12:14:01 +0600

    Initialize project and add Entity and basic Controller

---
+++ src/main/java/com/selflearning/journal/journal_backend/journalApplication.java
@@ -0,0 +1,13 @@
+package com.selflearning.journal.journal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class journalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(journalApplication.class, args);
+	}
+
+}
