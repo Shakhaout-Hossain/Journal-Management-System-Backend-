9d53c41ad5832893382e412be892ab07f0267165
Rimon <rimonruet@gmail.com>
2025-04-23 16:54:21 +0600

    mongo cluster intregate

---
+++ src/main/java/com/selflearning/journal/journal_backend/config/TransactionConfig.java
@@ -0,0 +1,7 @@
+package com.selflearning.journal.journal_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TransactionConfig {
+}
--- src/main/java/com/selflearning/journal/journal_backend/journalApplication.java
+++ src/main/java/com/selflearning/journal/journal_backend/journalApplication.java
@@ -2,12 +2,23 @@ package com.selflearning.journal.journal_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class journalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(journalApplication.class, args);
+	}
+
+	@Bean
+	public PlatformTransactionManager add(MongoDatabaseFactory mongoDatabaseFactory){
+		return new MongoTransactionManager(mongoDatabaseFactory);
 	}
 
 }
